17bfdff35406457f4acee59295ada61e2f154309
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2024-07-03 11:11:51 +0700

    loading page

--- src/pages/loading_page.jsx
+++ src/pages/loading_page.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
-import { Box, CircularProgress } from '@mui/material';
+import { Box } from '@mui/material';
 
-
+import './loading.css';
 
 const LoadingPage = () => (
-    <Box
-        sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh'
-        }}
-    >
-        <CircularProgress size={60} />
-    </Box>
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}
+  >
+    {/* <CircularProgress size={60} /> */}
+    <div className="cell">
+      <div className="card">
+        <span className="flower-loader">Loading…</span>
+      </div>
+    </div>
+  </Box>
 );
 
 export default LoadingPage;
