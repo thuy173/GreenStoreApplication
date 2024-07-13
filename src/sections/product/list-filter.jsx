e174567b53413a58368b56a894465a994f7f72d5
Tạ Thị Thanh Thủy <tathanhthuy17032004@gmail.com>
2024-07-14 00:26:17 +0700

    feat: isNew product

--- src/sections/product/list-filter.jsx
+++ src/sections/product/list-filter.jsx
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
+import { parseISO, differenceInDays } from 'date-fns';
 
 import { Box, Card, Grid, Stack, Button, Typography, CardContent } from '@mui/material';
 
@@ -12,6 +13,7 @@ import CustomSnackbar from 'src/components/snackbar/snackbar';
 // ----------------------------------------------------------------------
 
 export default function ProductListFilter({ productFilterData }) {
+  const currentDate = new Date();
   const [alert, setAlert] = useState({ message: null, severity: 'success', isOpen: false });
   const [cartItemCount, setCartItemCount] = useState(() => {
     const storedCount = parseInt(localStorage.getItem('cartItemCount'), 10);
@@ -74,143 +76,154 @@ export default function ProductListFilter({ productFilterData }) {
 
   return (
     <Grid container spacing={2} p={3}>
-      {productFilterData.map((items, index) => (
-        <Grid item xs={12} sm={8} md={6} lg={4} key={index}>
-          <Box
-            sx={{
-              position: 'relative',
-              '&:hover': {
-                '& $cardOverlay': {
-                  opacity: 0.5,
-                },
-                '& img': {
-                  transform: 'scale(1.08)',
-                },
-              },
-            }}
-          >
+      {productFilterData.map((items, index) => {
+        const createAtDate = parseISO(items.createAt);
+        const isNewProduct = differenceInDays(currentDate, createAtDate) < 2;
+        return (
+          <Grid item xs={12} sm={8} md={6} lg={4} key={index}>
             <Box
               sx={{
-                position: 'absolute',
-                top: 0,
-                right: '30%',
-                backgroundColor: '#507c5c',
-                color: '#fff',
-                padding: '4px 12px',
-                borderRadius: '0 16px 0 16px',
-                fontWeight: 'bold',
-                zIndex: 1,
-                '@media (max-width: 1508px)': {
-                  right: '12%',
-                },
-                '@media (max-width: 767px)': {
-                  right: '40%',
-                },
-                '@media (max-width: 991px)': {
-                  right: '25%',
-                },
-              }}
-            >
-              -10%
-            </Box>
-
-            <Card
-              sx={{
                 position: 'relative',
-                maxWidth: 325,
-                width: 345,
-                borderRadius: 2,
-                backgroundColor: 'rgba(248, 250, 250, 1)',
-                overflow: 'hidden',
-                '@media (max-width: 1508px)': {
-                  width: 260,
-                },
                 '&:hover': {
-                  cursor: 'pointer',
+                  '& $cardOverlay': {
+                    opacity: 0.5,
+                  },
+                  '& img': {
+                    transform: 'scale(1.08)',
+                  },
                 },
               }}
             >
-              <Link
-                href={`detail/${items.productId}`}
-                style={{ textDecoration: 'none', color: 'inherit' }}
-              >
-                <CardContent>
-                  <Typography
-                    marginLeft={1}
-                    marginTop={3}
-                    variant="h6"
-                    component="div"
-                    gutterBottom
-                  >
-                    {items.productName}
-                  </Typography>
-                  <Typography marginLeft={1} variant="caption" display="block" gutterBottom noWrap>
-                    {items.description}
-                  </Typography>
-                </CardContent>
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                  <Stack direction="column" alignItems="start" marginLeft={3} paddingBottom={10}>
-                    <Typography variant="body2" sx={{ padding: 0 }}>
-                      Price:
-                    </Typography>
-                    <Typography variant="h5" sx={{ padding: 0, fontWeight: 'bold' }}>
-                      ${items.price}
-                    </Typography>
-                    <Typography variant="caption" sx={{ padding: 0 }}>
-                      per <span>{items.unitOfMeasure}</span>
-                    </Typography>
-                  </Stack>
-
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      width: '168px',
-                      height: '168px',
-                      marginRight: 2,
-                      paddingTop: 1,
-                      overflow: 'hidden',
-                    }}
-                  >
-                    <Box
-                      component="img"
-                      src={
-                        items.productImages[0]?.imageUrl ||
-                        'https://res.cloudinary.com/dmmk9racr/image/upload/v1719892453/cat-1_q49n2j.png'
-                      }
-                      alt={items.productName}
-                      sx={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'contain',
-                      }}
-                    />
-                  </Box>
-                </Stack>
-              </Link>
-              <Stack direction="column" justifyContent="center" alignItems="center" margin={3}>
-                <Button
-                  variant="contained"
+              {isNewProduct && (
+                <Box
                   sx={{
-                    backgroundColor: '#d6e5d8',
-                    width: '150px',
-                    color: '#26643b',
-                    borderRadius: 1.4,
-                    '&:hover': {
-                      backgroundColor: '#26643b',
-                      color: '#d6e5d8',
+                    position: 'absolute',
+                    top: 0,
+                    right: '30%',
+                    backgroundColor: '#507c5c',
+                    color: '#fff',
+                    padding: '4px 12px',
+                    borderRadius: '0 16px 0 16px',
+                    fontWeight: 'bold',
+                    zIndex: 1,
+                    '@media (max-width: 1508px)': {
+                      right: '12%',
+                    },
+                    '@media (max-width: 767px)': {
+                      right: '40%',
+                    },
+                    '@media (max-width: 991px)': {
+                      right: '25%',
                     },
                   }}
-                  onClick={() => handleAddCart(items.productId)}
                 >
-                  Add to cart
-                </Button>
-              </Stack>
-            </Card>
-          </Box>
-        </Grid>
-      ))}
+                  New
+                </Box>
+              )}
+              <Card
+                sx={{
+                  position: 'relative',
+                  maxWidth: 325,
+                  width: 345,
+                  borderRadius: 2,
+                  backgroundColor: 'rgba(248, 250, 250, 1)',
+                  overflow: 'hidden',
+                  '@media (max-width: 1508px)': {
+                    width: 260,
+                  },
+                  '&:hover': {
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                <Link
+                  href={`detail/${items.productId}`}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                  <CardContent>
+                    <Typography
+                      marginLeft={1}
+                      marginTop={3}
+                      variant="h6"
+                      component="div"
+                      gutterBottom
+                    >
+                      {items.productName}
+                    </Typography>
+                    <Typography
+                      marginLeft={1}
+                      variant="caption"
+                      display="block"
+                      gutterBottom
+                      noWrap
+                    >
+                      {items.description}
+                    </Typography>
+                  </CardContent>
+                  <Stack direction="row" alignItems="center" justifyContent="space-between">
+                    <Stack direction="column" alignItems="start" marginLeft={3} paddingBottom={10}>
+                      <Typography variant="body2" sx={{ padding: 0 }}>
+                        Price:
+                      </Typography>
+                      <Typography variant="h5" sx={{ padding: 0, fontWeight: 'bold' }}>
+                        ${items.price}
+                      </Typography>
+                      <Typography variant="caption" sx={{ padding: 0 }}>
+                        per <span>{items.unitOfMeasure}</span>
+                      </Typography>
+                    </Stack>
+
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '168px',
+                        height: '168px',
+                        marginRight: 2,
+                        paddingTop: 1,
+                        overflow: 'hidden',
+                      }}
+                    >
+                      <Box
+                        component="img"
+                        src={
+                          items.productImages[0]?.imageUrl ||
+                          'https://res.cloudinary.com/dmmk9racr/image/upload/v1719892453/cat-1_q49n2j.png'
+                        }
+                        alt={items.productName}
+                        sx={{
+                          width: '100%',
+                          height: '100%',
+                          objectFit: 'contain',
+                        }}
+                      />
+                    </Box>
+                  </Stack>
+                </Link>
+                <Stack direction="column" justifyContent="center" alignItems="center" margin={3}>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      backgroundColor: '#d6e5d8',
+                      width: '150px',
+                      color: '#26643b',
+                      borderRadius: 1.4,
+                      '&:hover': {
+                        backgroundColor: '#26643b',
+                        color: '#d6e5d8',
+                      },
+                    }}
+                    onClick={() => handleAddCart(items.productId)}
+                  >
+                    Add to cart
+                  </Button>
+                </Stack>
+              </Card>
+            </Box>
+          </Grid>
+        );
+      })}
       <CustomSnackbar
         open={alert.isOpen}
         onClose={handleCloseAlert}
